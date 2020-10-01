cb9e8528a66feec76aba0ea0e8757c5fa11b8a81
AbhilipsaPati <72179723+AbhilipsaPati@users.noreply.github.com>
2020-10-01 09:00:48 +0530

    Add files via upload

---
+++ 1stint.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+ int main()
+{
+	int no = 20;
+	printf ("The value is %d",no);
+}
---
+++ add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int no1,no2,res;
+	printf("Enter values");
+	scanf("%d%d",&no1,&no2);
+	res = no1+no2;
+	printf("The sum is %d",res);
+}
