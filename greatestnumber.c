de0e38649d873f2b26582d7183cf25377b5b26b3
Lipsa-Chinmayee-Hota <69186836+Lipsa-Chinmayee-Hota@users.noreply.github.com>
2021-07-22 23:35:26 +0530

    Add files via upload

---
+++ greatestnumber.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+
+{
+
+int ch=3;
+
+switch(ch)
+
+{
+
+default: printf(“Default”);
+
+case 1: printf(“option1”); break;
+
+case 2: printf(“option2”); break;
+
+}
+
+return 0;
+
+}
+
+
+
